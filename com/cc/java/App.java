c68281af6696d2f3d4cbb7f3541b93058ae11d7d
Cullain <lukas.gedrat@gmx.net>
2023-06-01 10:45:45 +0200

    Output

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,5 +5,10 @@ public class App {
     public static void main(String[] args) {
      
     }
+ 
+    public static void output(String outStr) { 
+        System.out.println(outStr);
     
+    }
+
 }
